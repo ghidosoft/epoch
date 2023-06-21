aff75b42941e374d55f6cc1059c87f94b9b98fee
Andrea Ghidini <andrea.ghidini@looptribe.com>
2023-06-21 15:39:14 +0200

    Palette constructor

--- src/core/src/Palette.cpp
+++ src/core/src/Palette.cpp
@@ -2,6 +2,12 @@
 
 namespace epoch
 {
+    Palette::Palette(std::span<Color> colors)
+    {
+        m_palette.resize(colors.size());
+        m_palette.assign(colors.begin(), colors.end());
+    }
+
     Color Palette::map(const std::size_t index) const
     {
         return m_palette[index];
