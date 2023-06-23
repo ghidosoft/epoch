92bfad4055aa72e7a393de05119bc35ddc65e4b5
Andrea Ghidini <andrea.ghidini@looptribe.com>
2023-06-23 12:14:19 +0200

    License info in source files

--- src/zxspectrum/src/Z80Cpu.h
+++ src/zxspectrum/src/Z80Cpu.h
@@ -1,3 +1,19 @@
+/* This file is part of Epoch, Copyright (C) 2023 Andrea Ghidini.
+ *
+ * Epoch is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Epoch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Epoch.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef SRC_EPOCH_ZXSPECTRUM_Z80CPU_H_
 #define SRC_EPOCH_ZXSPECTRUM_Z80CPU_H_
 
