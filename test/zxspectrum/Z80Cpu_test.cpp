d5473bb4402f1f30869173181dda6a5c6701dcfb
Andrea Ghidini <andrea.ghidini@looptribe.com>
2023-07-06 17:51:06 +0200

    First instruction test

--- test/zxspectrum/Z80Cpu_test.cpp
+++ test/zxspectrum/Z80Cpu_test.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 
 #include <array>
+#include <cstring>
+#include <span>
 
 #include "../../src/zxspectrum/src/Z80Cpu.h"
 
@@ -9,6 +11,13 @@ namespace epoch::zxspectrum
     class TestZ80Interface : public Z80Interface
     {
     public:
+        TestZ80Interface() = default;
+        TestZ80Interface(std::span<const uint8_t> initRam)
+        {
+            assert(initRam.size_bytes() <= m_ram.size());
+            std::memcpy(m_ram.data(), initRam.data(), initRam.size_bytes());
+        }
+
         uint8_t read(const uint16_t address) override
         {
             return m_ram[address];
@@ -23,10 +32,22 @@ namespace epoch::zxspectrum
     };
 
     TEST(Z80Cpu, ResetBehavior) {
-        TestZ80Interface ram{};
-        Z80Cpu sut{ ram };
+        TestZ80Interface bus{};
+        Z80Cpu sut{ bus };
         sut.reset();
         EXPECT_EQ(sut.registers().pc, 0);
+    }
+
+    TEST(Z80Cpu, Opcode01xxxxxx_LD_C_B) {
+        TestZ80Interface bus{ std::initializer_list<uint8_t>{ 0x48 } };
+        Z80Cpu sut{ bus };
+        sut.registers().bc.value = 0x00aa;
+        sut.clock();
+        sut.clock();
+        sut.clock();
+        sut.clock();
+        EXPECT_EQ(sut.registers().pc, 1);
+        EXPECT_EQ(sut.registers().bc.value, 0xaa00);
     }
 
 }
